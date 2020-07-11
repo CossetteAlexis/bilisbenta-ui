cdb10f7c4196839d33e5134f5febc6ffccc031f9
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-07-11 16:39:33 +0800

    Update main.js

--- bilisbenta/src/main.js
+++ bilisbenta/src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import AOS from 'aos';
-import './aos/dist/aos.css';
+import 'aos/dist/aos.css';
 
 Vue.config.productionTip = false
 
 new Vue({
-    create() {
+    created() {
         AOS.init();
     },
     router,
